b3efe7e5dd6638cde5a1d8993daf25420a788e68
Huy Tran <huy.hoang2225@gmail.com>
2023-08-18 14:14:27 +0700

    Commit project

---
+++ XFColorPickerControl/XFColorPickerControl/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFColorPickerControl
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+		private void ColorPicker_PickedColorChanged(object sender, Color colorPicked)
+		{
+			// Use the selected color
+			SelectedColorDisplayFrame.BackgroundColor = colorPicked;
+			SelectedColorValueLabel.Text = colorPicked.ToHex();
+			ColorPickerHolderFrame.BackgroundColor = colorPicked;
+
+			if (colorPicked.Luminosity < 0.5)
+				SelectedColorValueLabel.TextColor = Xamarin.Forms.Color.White;
+			else
+				SelectedColorValueLabel.TextColor = Xamarin.Forms.Color.SlateGray;
+		}
+	}
+}
